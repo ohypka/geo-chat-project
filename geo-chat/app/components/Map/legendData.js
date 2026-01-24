c59469489a873c0ebb268d7cfde1777e0c961171
Julia <julka.uujma@gmail.com>
2026-01-24 15:16:37 +0100

    ui(map): map legend in Polish

--- geo-chat/app/components/Map/legendData.js
+++ geo-chat/app/components/Map/legendData.js
@@ -1,52 +1,52 @@
 export const LEGENDS = {
     bikes: {
-        title: "Bike stations",
+        title: "Stacje rowerowe",
         items: [
             {
                 color: "#00c71b",
-                label: "Bikes available (more than 3)"
+                label: "Dostępne rowery (więcej niż 3)"
             },
             {
                 color: "#e3b707",
-                label: "Limited availability (1–3 bikes)"
+                label: "Ogrniczona dostępność (1–3 rowery)"
             },
             {
                 color: "#dd2828",
-                label: "No bikes available"
+                label: "Brak dostępnych rowerów"
             }
         ],
     },
     traffic: {
-        title: "Traffic congestion",
+        title: "Ruch drogowy",
         items: [
             {
                 color: "#008000",
-                label: "Low congestion (near free-flow traffic)"
+                label: "Małe natężenie ruchu"
             },
             {
                 color: "#FFA500",
-                label: "Moderate congestion (reduced traffic speed)"
+                label: "Umiarkowane natężenie ruchu"
             },
             {
                 color: "#FF0000",
-                label: "Heavy congestion (severe slowdown)"
+                label: "Duże natężenie ruchu"
             }
         ]
     },
     doctors: {
-        title: "Doctors – waiting time",
+        title: "Lekarze – czas oczekiwania",
         items: [
             {
                 color: "#00c71b",
-                label: "Short waiting time (less than 5 days)"
+                label: "Krótki czas oczekiwania (mniej niż 5 dni)"
             },
             {
                 color: "#e3b707",
-                label: "Moderate waiting time (5–13 days)"
+                label: "Średni czas oczekiwania (5–13 dni)"
             },
             {
                 color: "#dd2828",
-                label: "Long waiting time (14 days or more)"
+                label: "Długi czas oczekiwania (więcej niż 13 dni)"
             }
         ]
     }
